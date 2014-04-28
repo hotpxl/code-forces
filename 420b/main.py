e9ae80ae1a9fddce8bd78209d92079be70055a7f
Kevin Lee <kevinleeone@gmail.com>
2014-04-28 21:25:59 +0800

    [master] Limit of my python skills

--- 420b/main.py
+++ 420b/main.py
@@ -2,36 +2,37 @@ import sys
 
 if __name__ == '__main__':
     participantNum, messageNum = map(int, sys.stdin.readline().split())
-    messages = [sys.stdin.readline().split() for i in range(messageNum)]
-    for i in messages:
-        i[1] = int(i[1])
-    nonExistent = ((1 << participantNum) - 1) << 1
     online = 0
-    offlineAtFirst = 0
-    for i in messages:
-        if i[0] == '+':
-            offlineAtFirst |= (1 << i[1])
-        else:
-            if not offlineAtFirst & (1 << i[1]):
-                online |= (1 << i[1])
-    nonExistent &= ~(online | offlineAtFirst)
-    possible = ((1 << participantNum) - 1) << 1
-    offline = possible & ~(nonExistent | online)
-    if online:
-        possible &= ~offline
-    for i in messages:
-        num = (1 << i[1])
-        if i[0] == '+':
-            offline &= ~num
+    notPresent = ((1 << participantNum) - 1) << 1
+    flag, num = sys.stdin.readline().split()
+    num = 1 << int(num)
+    notPresent &= ~num
+    possible = num
+    if flag == '+':
+        online |= num
+    for i in range(messageNum - 1):
+        flag, num = sys.stdin.readline().split()
+        num = 1 << int(num)
+        notPresent &= ~num
+        if flag == '+':
+            if not online and num != possible:
+                possible = 0
             online |= num
-            if online:
-                possible &= ~offline
         else:
-            offline |= num
-            online &= ~num
-            if online:
-                possible &= ~num
-    possible |= nonExistent
+            if num != possible:
+                if not num & online:
+                    if not online:
+                        possible = num
+                    else:
+                        possible = 0
+                online &= ~num
+            else:
+                online &= ~num
+                if online != 0:
+                    possible = 0
+    if online and not possible & online:
+        possible = 0
+    possible |= notPresent
     if possible:
         counter = 0
         boss = []
