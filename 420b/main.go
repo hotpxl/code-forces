b1857eb0bf58ad06beaf9d2a0c1d697d509bd442
Yutian Li <kevinleeone@gmail.com>
2014-05-04 17:09:01 +0800

    [master] WIP

--- 420b/main.go
+++ 420b/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-  "os"
-  "bufio"
   "fmt"
-  "strings"
-  "strconv"
 )
 
 const maxIndex = 1563
 
 type Status [maxIndex]int64
 
-func (s *Status) init(num uint) {
+func (s *Status) init(num uint32) {
   high := num / 64
   for i := high; 0 < i; i-- {
     s[i] = -1
@@ -25,17 +21,10 @@ func (s *Status) init(num uint) {
 }
 
 func main() {
-  bio := bufio.NewReader(os.Stdin)
-  str, _ := bio.ReadString('\n')
-  strSplit := strings.Fields(str)
-  participantNum, _ := strconv.ParseUint(strSplit[0], 0, 0)
-  messageNum := strSplit[1]
-  online := new(Status)
-  notPresent := new(Status)
-  str, _ = bio.ReadString('\n')
-  strSplit = strings.Fields(str)
-  flag := strSplit[0]
-  num, _ := strconv.ParseUint(strSplit[1], 0, 0)
-  num = 1 << num
-  fmt.Println(participantNum, messageNum)
+  var participantNum, messageNum int32
+  var online, notPresent Status
+  fmt.Scanf("%d %d", &participantNum, &messageNum)
+  notPresent.init(uint32(participantNum))
+  _ = online
+  fmt.Println(notPresent[0])
 }
