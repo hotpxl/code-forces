24750c50c8e4a42434826a6c8c312fabd34982e4
Kevin Lee <kevinleeone@gmail.com>
2014-05-05 13:35:27 +0800

    [master] 426a

---
+++ 426a/main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+if __name__ == '__main__':
+    n, s = map(int, sys.stdin.readline().split())
+    mugs = list(sorted(map(int, sys.stdin.readline().split())))
+    if sum(mugs[:n - 1]) <= s:
+        print('YES')
+    else:
+        print('NO')
+
