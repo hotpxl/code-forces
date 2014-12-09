0146d0810ef8b2dfc7977eea906d0e8d1c7240b7
Yutian Li <hotpxless@gmail.com>
2014-12-10 03:20:17 +0800

    [master] Too slow. Have to go with bitwise.

--- 殘念/420b/main.ml
+++ 殘念/420b/main.ml
@@ -1,19 +1,4 @@
-open Set
-
 exception InvalidInput
-
-module IntSet = Set.Make(
-  struct
-    let compare = Pervasives.compare
-    type t = int
-  end
-)
-
-type status =
-  | Uninitialized
-  | FoundOnline of int
-  | FoundOffline of int
-  | NotFound
 
 let (|>) x f = f x
 
@@ -23,15 +8,85 @@ let rec makeList = fun len f ->
   else
     (f ()) :: (makeList (len - 1) f)
 
-let rec range = fun s e ->
-  if s < e then
-    s :: range (s + 1) e
-  else
-    []
+let findAppearance = fun records n ->
+  let all = Big_int.pred_big_int (Big_int.shift_left_big_int Big_int.unit_big_int (n + 1)) in
+  let show = List.fold_left (fun accum (_, id) ->
+      Big_int.shift_left_big_int Big_int.unit_big_int id
+      |> Big_int.or_big_int accum
+    ) Big_int.unit_big_int
+  in
+  (show, Big_int.sub_big_int all show)
 
-let findHidden = fun records n ->
+let findXMan = fun records n ->
+  List.fold_left (fun (onlineSet, x, xOnline) (goOnline, id) ->
+    if goOnline then
+      let onlineSet' =
+        Big_int.shift_left_big_int Big_int.unit_big_int id
+        |> Big_int.or_big_int onlineSet
+      in
+      let x' = if id <> x && not xOnline then -1 else x in
+      let xOnline' = if id = x then true else xOnline in
+      (onlineSet', x', xOnline')
+    else
+      let onlineSet' = IntSet.remove id onlineSet in
+      let onlineSet' =
+        Big_int.shift_left_big_int Big_int.unit_big_int id
+        |>
+      let exists = IntSet.exists (fun i -> i = id) onlineSet in
+      let nowEmpty = IntSet.is_empty onlineSet' in
+      let x' =
+        if exists then
+          x
+        else
+          if nowEmpty then
+            id
+          else
+            -1
+      in
+      let xOnline' = if id = x || not exists then false else xOnline in
+      (onlineSet', x', xOnline')
+  ) (IntSet.empty, 0, true) records
 
+let findNormalLeader = fun records possible ->
+  List.fold_left (fun (onlineSet, possible) (goOnline, id) ->
+    if goOnline then
+      let onlineSet' = IntSet.add id onlineSet in
+      let possible' =
+        if not (IntSet.is_empty onlineSet) then
+          IntSet.remove id possible
+        else
+          IntSet.singleton id
+          |> IntSet.inter possible
+      in
+      (onlineSet', possible')
+    else
+      let onlineSet' = IntSet.remove id onlineSet in
+      let possible' =
+        if not (IntSet.is_empty onlineSet') then
+          IntSet.remove id possible
+        else
+          possible
+      in
+      (onlineSet', possible')
+  ) (IntSet.empty, possible) records
 
+let printRes = fun result ->
+  let l = IntSet.fold (fun ele l ->
+    ele :: l
+  ) result []
+  |> List.rev
+  in
+  print_int (List.length l)
+  |> print_newline;
+  List.iter (fun ele -> Printf.printf "%d " ele) l
+
+let rec printShit = fun shit ->
+  match shit with
+  | (a, b) :: tl ->
+    Printf.printf "%B %d\n" a b;
+    printShit tl
+  | _ ->
+    ()
 
 let () =
   let (n, m) = read_line ()
@@ -49,10 +104,22 @@ let () =
     |> Str.split (Str.regexp " +")
     |> fun li -> match li with
       | a :: [b] ->
-        ((if a = "+" then 1 else 0), (int_of_string b))
+        (a = "+", int_of_string b)
       | _ ->
         raise InvalidInput
   )
+  |> List.rev
   in
-  ignore (find_never_appeared records);
-  ()
+  let (show, hidden) = findAppearance records n in
+  let (_, xMan, _) = findXMan records in
+  (* Not found *)
+  (match xMan with
+  | 0 ->
+    let (_, ld) = findNormalLeader records show in
+    printRes (IntSet.union hidden ld)
+  | -1 ->
+    printRes hidden
+  | _ ->
+    printRes (IntSet.add xMan hidden)
+  )
+  |> print_newline
