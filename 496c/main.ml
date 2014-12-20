5d3c380cea18e2e3c5a50fd5bbcfd244be506b2a
Yutian Li <hotpxless@gmail.com>
2014-12-20 23:31:20 +0800

    [master] Check

--- 496c/main.ml
+++ 496c/main.ml
@@ -1,5 +1,9 @@
 exception Invalid_input
 
+type status =
+  | Stable
+  | Safe
+
 let (|>) x f = f x
 
 let rec make_list = fun len f ->
@@ -38,6 +42,50 @@ let read_input = fun n m ->
   make_list m (fun () -> [])
   |> do_read_input 0
 
+let process_one_column = fun status chars ->
+  let rec need_to_remove = fun s_list c_list ->
+    match s_list, c_list with
+    | h1 :: (m1 :: t1 as p1), h2 :: (m2 :: t2 as p2) ->
+      if h1 = Stable && h2 < m2 then
+        true
+      else
+        need_to_remove p1 p2
+    | _ ->
+      false
+  in
+  if need_to_remove status chars then
+    status, true
+  else
+    let rec get_new_status = fun s_list c_list ->
+      match s_list, c_list with
+      | h1 :: (m1 :: t1 as p1), h2 :: (m2 :: t2 as p2) ->
+        let new_s =
+          if h1 = Safe || m2 < h2 then
+            Safe
+          else
+            Stable
+        in
+        new_s :: get_new_status p1 p2
+      | hd :: [], _ ->
+        [hd]
+      | _ ->
+        raise Invalid_input
+    in
+    (get_new_status status chars), false
+
+let process_all_columns = fun n input ->
+  let initial = make_list n (fun () -> Stable) in
+  let rec do_process = fun status count input ->
+    match input with
+    | hd :: tl ->
+      let new_status, removed = process_one_column status hd in
+      let new_count = if removed then count + 1 else count in
+      do_process new_status new_count tl
+    | _ ->
+      count
+  in
+  do_process initial 0 input
+
 let () =
   let (n, m) =
     read_line ()
@@ -49,4 +97,6 @@ let () =
         | _ -> raise Invalid_input
   in
   let input = read_input n m in
-  ignore (List.map (List.map print_char) input)
+  process_all_columns n input
+  |> print_int
+  |> print_newline
